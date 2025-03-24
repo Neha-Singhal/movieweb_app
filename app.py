1e8086cdbd41ab8df3facaf3ffc2d8f07e3a456e
neha <nehasinghal@Nehas-MacBook-Air.local>
2025-03-24 20:45:28 +0100

    plan our route map

--- app.py
+++ app.py
@@ -1,25 +1,132 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash
 from datamanager.sqlite_data_manager import SQLiteDataManager
 
 app = Flask(__name__)
-data_manager = SQLiteDataManager('instance/moviwebapp.db')
+app.secret_key = 'your_secret_key'  # Required for flashing messages
+data_manager = SQLiteDataManager('instance/moviweb.db')
 
+# Home Route
 @app.route('/')
 def home():
-     """Welcome to Movie App"""
-     return render_template('index.html')
+    """
+    WELCOME to movieweb app
+    """
+    return render_template('index.html')
 
+# Users List Route
 @app.route('/users')
-def list_users():
+def users_list():
+    """
+    Display a list of all users.
+    """
     users = data_manager.get_all_users()
-    return render_template('users.html')
+    return render_template('users.html', users=users)
 
+# User Movies Route
+@app.route('/users/<int:user_id>')
+def user_movies(user_id):
+    """
+    Display a specific user's list of favorite movies.
+    """
+    user = data_manager.get_user_by_id(user_id)
+    if not user:
+        flash("User not found!", "error")
+        return redirect(url_for('users_list'))
+    movies = data_manager.get_user_movies(user_id=user_id)
+    return render_template('user_movies.html', user=user, movies=movies)
 
+# Add User Route
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    """
+    Add a new user to the application.
+    """
+    if request.method == 'POST':
+        name = request.form.get('name')
+        if name:
+            data_manager.add_user(name=name)
+            flash("User added successfully!", "success")
+            return redirect(url_for('users_list'))
+        else:
+            flash("Name cannot be empty!", "error")
+    return render_template('add_user.html')
 
+# Add Movie Route
+@app.route('/users/<int:user_id>/add_movie', methods=['GET', 'POST'])
+def add_movie(user_id):
+    """
+    Add a new movie to a user's list of favorite movies.
+    """
+    user = data_manager.get_user_by_id(user_id)
+    if not user:
+        flash("User not found!", "error")
+        return redirect(url_for('users_list'))
 
+    if request.method == 'POST':
+        name = request.form.get('name')
+        director = request.form.get('director')
+        year = request.form.get('year')
+        rating = request.form.get('rating')
 
+        if name and director and year and rating:
+            data_manager.add_movie(user_id=user_id, name=name, director=director, year=year, rating=rating)
+            flash("Movie added successfully!", "success")
+            return redirect(url_for('user_movies', user_id=user_id))
+        else:
+            flash("All fields are required!", "error")
 
+    return render_template('add_movie.html', user=user)
 
+# Update Movie Route
+@app.route('/users/<int:user_id>/update_movie/<int:movie_id>', methods=['GET', 'POST'])
+def update_movie(user_id, movie_id):
+    """
+    Update details of a specific movie in a user's list.
+    """
+    user = data_manager.get_user_by_id(user_id)
+    if not user:
+        flash("User not found!", "error")
+        return redirect(url_for('users_list'))
+
+    movie = data_manager.get_movie_by_id(movie_id)
+    if not movie or movie.user_id != user_id:
+        flash("Movie not found!", "error")
+        return redirect(url_for('user_movies', user_id=user_id))
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        director = request.form.get('director')
+        year = request.form.get('year')
+        rating = request.form.get('rating')
+
+        if name and director and year and rating:
+            data_manager.update_movie(movie_id=movie_id, name=name, director=director, year=year, rating=rating)
+            flash("Movie updated successfully!", "success")
+            return redirect(url_for('user_movies', user_id=user_id))
+        else:
+            flash("All fields are required!", "error")
+
+    return render_template('update_movie.html', user=user, movie=movie)
+
+# Delete Movie Route
+@app.route('/users/<int:user_id>/delete_movie/<int:movie_id>')
+def delete_movie(user_id, movie_id):
+    """
+    Delete a specific movie from a user's list.
+    """
+    user = data_manager.get_user_by_id(user_id)
+    if not user:
+        flash("User not found!", "error")
+        return redirect(url_for('users_list'))
+
+    movie = data_manager.get_movie_by_id(movie_id)
+    if not movie or movie.user_id != user_id:
+        flash("Movie not found!", "error")
+        return redirect(url_for('user_movies', user_id=user_id))
+
+    data_manager.delete_movie(movie_id=movie_id)
+    flash("Movie deleted successfully!", "success")
+    return redirect(url_for('user_movies', user_id=user_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- datamanager/sqlite_data_manager.py
+++ datamanager/sqlite_data_manager.py
@@ -7,3 +7,46 @@ class SQLiteDataManager(DataManagerInterface):
     def __init__(self, db_file_name):
         self.engine = create_engine(f'sqlite:///{db_file_name}')
         self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+        def get_all_users(self):
+            return self.session.query(User).all()
+
+        def get_user_movies(self, user_id):
+            return self.session.query(Movie).filter_by(user_id=user_id).all()
+
+        def add_user(self, name):
+            user = User(name=name)
+            self.session.add(user)
+            self.session.commit()
+            return user
+
+        def add_movie(self, user_id, name, director, year, rating):
+            movie = Movie(name=name, director=director, year=year, rating=rating, user_id=user_id)
+            self.session.add(movie)
+            self.session.commit()
+            return movie
+
+        def update_movie(self, movie_id, name=None, director=None, year=None, rating=None):
+            movie = self.session.query(Movie).get(movie_id)
+            if movie:
+                if name:
+                    movie.name = name
+                if director:
+                    movie.director = director
+                if year:
+                    movie.year = year
+                if rating:
+                    movie.rating = rating
+                self.session.commit()
+            return movie
+
+        def delete_movie(self, movie_id):
+            movie = self.session.query(Movie).get(movie_id)
+            if movie:
+                self.session.delete(movie)
+                self.session.commit()
+            return movie
+
+        def close(self):
+            self.session.close()
